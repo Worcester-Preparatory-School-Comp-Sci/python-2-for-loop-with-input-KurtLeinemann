9ee91256ae8a20efacf643a25616f95a2fcd7928
Colleen McGuire <mzmacky2@gmail.com>
2019-10-01 14:57:44 -0400

    good work... see comment in code

--- PigLatin.py
+++ PigLatin.py
@@ -3,6 +3,6 @@
 word=input("Pick a word")
 vowelList=['a','e','i','o','u']
 if word[0] in vowelList:
-	print(print(word[0:]+"yay"))
+	print(word +"yay")  #yikes... print(print... also didn't need to select a slice of word.. just whole word
 else:
-	print(word[1:]+word[0]+"ay")
+	print(word[1:] + word[0] + "ay")
